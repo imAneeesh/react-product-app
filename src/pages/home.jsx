bc5773137f1793112573038e6c252ca6395b122b
Aneesh <aneeshad2@gmail.com>
2023-06-16 21:05:01 +0530

    .

---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <div className="container">
+                <h2>hello from Home</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/pages/login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Login = () => {
+    return (
+        <div className='home'>
+            <div className="container">
+                Hello from login
+            </div>
+        </div>
+    )
+}
+
+export default Login
